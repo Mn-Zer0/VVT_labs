49e3ca0cfa5b3dd47a6483e30c0c650ee5cc1f8e
Mn_Zer0 <35655185+OptiZevs@users.noreply.github.com>
2023-05-23 20:45:22 +0300

    Update main.py

--- Лабораторная работа 2/main.py
+++ Лабораторная работа 2/main.py
@@ -10,17 +10,23 @@ s1 = r.get("http://api.openweathermap.org/data/2.5/forecast", params={'q': city_
                                                                       'APPID': API_key})
 m1 = s1.json()
 
-print('Прогноз на день:')
+print('Прогноз на неделю:')
 for i in m1['list']:
-    print(i['dt_txt'], i['wind']['speed'], i['visibility'])
+    print('Дата:', i['dt_txt'] )
+    print('Скорость ветра:', i['wind']['speed'])
+    print('Видимость:', i['visibility'])
     print('-' * 30)
 
 
 s2 = r.get("http://api.openweathermap.org/data/2.5/weather", params={'q': city_name, 'units': 'metric', 'lang': 'ru',
                                                                      'APPID': API_key})
-m2 = s1.json()
-
-print('Прогноз на неделю:')
-for i in m2['list']:
-    print(i['dt_txt'], i['wind']['speed'], i['visibility'])
-    print('-' * 30)
+m2 = s2.json()
+print('-' * 30)
+print("TEST")
+print(s2.text)
+print(m2)
+print('-' * 30)
+print('Прогноз на день:')
+print('Скорость ветра:', m2['wind']['speed'])
+print('Видимость:', m2['visibility'])
+print('-' * 30)
